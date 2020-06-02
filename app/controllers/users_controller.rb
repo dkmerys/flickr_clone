8d02522fd099afa4a6305ffc2acc7a045efc6d80
Daniel Merys <dkmerys@gmail.com>
2020-06-02 11:56:06 -0700

    add user controller and some routing

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def index
+    puts "Something"
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "You've successfully signed up!"
+      session[:user_id] = @user_id
+      redirect_to '/signup'
+    else
+      flash[:alert] = "There was a problem signing up"
+      redirect_to '/signup'
+    end
+  end
+
+  private 
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :username)
+  end
+end
